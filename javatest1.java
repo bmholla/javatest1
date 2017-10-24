a03b1163a8f619008069a2d8578bd306b7429e6d
Brian Holland <bholland@majordomo.srccomputers.com>
2017-10-24 16:35:12 -0600

    added logic for easy personlist generation

--- javatest1.java
+++ javatest1.java
@@ -1,9 +1,52 @@
 
+import java.util.Random;
+
 public class javatest1
 {
     public static void main(String[] args)
     {
         for(String a : args)
             System.out.println(a);
+
+        Person[] persons = GenPersonList.genPersonList(10);
+
+        for(Person p : persons)
+            System.out.println(p.getAge());
+    }
+}
+
+class Person
+{
+    private String name = null;
+    private int age = 0;
+
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() { return age; }
+    public String getName() { return name; }
+
+    public void setAge(int age) { this.age = age; }
+    public void setName(String name) {this.name = name;}
+}
+
+class GenPersonList
+{
+    static Person[] genPersonList(int count)
+    {
+        Person[] personlist = new Person[count];
+
+        Random r = new Random();
+
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("got here");
+            personlist[i] = new Person("", r.nextInt(99));
+        }
+
+        return personlist;
     }
 }
